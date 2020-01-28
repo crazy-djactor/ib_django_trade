44a63571dfb2b7a02c290e3c6f88645aacd4113e
jason <babhan312@gmail.com>
2020-01-28 18:56:38 +0800

    static files fix4

--- source/app/conf/production/settings.py
+++ source/app/conf/production/settings.py
@@ -121,10 +121,10 @@ LANGUAGES = [
 TIME_ZONE = 'UTC'
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(CONTENT_DIR, 'static')
+STATIC_ROOT = os.path.join(CONTENT_DIR, 'static/')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(CONTENT_DIR, 'media')
+MEDIA_ROOT = os.path.join(CONTENT_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
